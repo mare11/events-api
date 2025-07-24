a0c5f1d3bf39e19329273681a8cbc6cfe8d54c64
Marko Jevtovic <markojevtovic25@yahoo.com>
2025-07-24 23:29:03 +0200

    add additional project configuration

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    plugins: { js, prettier: eslintPluginPrettier },
+    extends: ['js/recommended'],
+    rules: { 'prettier/prettier': 'error' }
+  },
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    languageOptions: { globals: globals.browser }
+  },
+  tseslint.configs.recommended
+]);
--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,11 @@
-export const hello = async (event) => {
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const hello: APIGatewayProxyHandler = async (event) => {
+  console.log(event);
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
+      message: 'Go Serverless v4! Your function executed successfully!'
+    })
   };
 };
